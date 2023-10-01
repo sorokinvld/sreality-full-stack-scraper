f1ba84bb13f4c537136cbeece88e1af8cf5261e0
Nikita <nikita.shalashkov@gmail.com>
2023-10-01 03:34:40 +0300

    Delete client/.next directory

--- client/.next/static/chunks/app/layout-dd0953073f0a5d84.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6232:function(n,e,u){Promise.resolve().then(u.t.bind(u,3177,23))},3177:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=6232)}),_N_E=n.O()}]);
